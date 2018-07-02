6267b4975d92310a62563feb9aac0b92efd18e72
silencerune <silencerune@gmail.com>
2018-07-02 23:55:41 +0800

    災禍主名詞解釋

--- Js/SelectElement.js
+++ Js/SelectElement.js
@@ -12,12 +12,14 @@ app.directive("selectElement", () => {
 });
 app.controller("selectElementController", ($rootScope, $scope, selectShareDate) => {
     $scope.story = ['竊凝語', '屏幕那端', '短篇集']
+    $scope.nowSelect = "Middle";
     $scope.select = {
         title: ['North', 'West', 'South', 'East', 'Police', 'Glossary'],
     }
     $rootScope.showNoti = true;
 
     $scope.slectplace = (place) => {
+        $scope.nowSelect = "Middle";
         selectShareDate.selectPlace(place);
         $scope.$broadcast('changeElement');
     }
